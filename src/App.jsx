f81451599e5d0e915c23642bd8c0c8df8af28c9e
Eduardo <el_alienx@hotmail.com>
2022-03-11 09:36:43 +0100

    removed unused files, and started to apply the design system

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// Project files
+import "styles/style.css"
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, sed
+        nostrum officiis dolores iusto consectetur nam tenetur eveniet hic
+        aspernatur illo quod soluta sint perspiciatis fugiat. Similique pariatur
+        dolore atque? Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        Velit, qui, iusto voluptate architecto nulla quasi sunt magni facere
+        veniam facilis aspernatur delectus nam illo dolorem incidunt quod in
+        nisi consectetur.
+      </p>
+    </div>
+  );
+}
